8c9119afc97169e9e7fb15f58eda06f35703fb00
66952546 <luuhienlong201@gmail.com>
2020-10-15 14:08:58 +0700

    Add the code base

---
+++ Assets/Scripts/Player/CharacterAnim.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+    //Public
+    public Animator animator;
+    public CharacterMovement movement;
+    public CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the character move (horizontal != 0), apply the running animation
+        if (movement.horizontal!=0)
+            animator.SetBool("isRunning", true);
+        else animator.SetBool("isRunning", false);
+
+        //If the character is NOT grounded, apply the jumping/falling animation
+        if (controller.m_Grounded == false)
+            animator.SetBool("isJumping", true);
+        else animator.SetBool("isJumping", false);
+
+        //The same for the dash
+        if (movement.dashSpeed > 1)
+            animator.SetBool("isDashing", true);
+        else
+        {
+            animator.SetBool("isDashing", false);
+        }
+
+        //The same for the attacking
+        animator.SetBool("isAttacking", movement.isAttacking);
+        
+    }
+}
---
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //Public
+    public CharacterController controller;
+    public float moveSpeed = 40f;
+    public float horizontal = 0f;
+    public bool ifJump = false;
+
+    public float dashRecharge = 1f;
+    private float rechargeTimer = 0f;
+    public float dashMultiplier = 10;
+    public float dashDuration = 1.0f;
+    private float dashTimer = 0f;
+    public float dashSpeed = 1f;
+
+    public bool isAttacking = false;
+    public float attackDuration = 1f;
+    private float attackTimer = 0f;
+
+    //Variables
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+            ifJump = true;
+        if (Input.GetButtonDown("Dash"))
+            dashSpeed = dashMultiplier;
+        if (Input.GetButtonDown("Attack"))
+            isAttacking = true;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (dashTimer >= dashDuration)      //If the player dashed for enough time
+        {
+            dashSpeed = 1;
+            dashTimer = 0;
+        }
+        else dashTimer = dashTimer + Time.deltaTime;
+
+        if (rechargeTimer >= dashRecharge)
+            rechargeTimer = 0;
+        else rechargeTimer = rechargeTimer + Time.deltaTime;
+
+        if (attackTimer >= attackDuration)
+        {
+            attackTimer = 0;
+            isAttacking = false;
+        }
+        else attackTimer = attackTimer + Time.deltaTime;
+
+        controller.Move(horizontal * dashSpeed * Time.fixedDeltaTime * moveSpeed, false, ifJump);
+        ifJump = false;
+        
+        
+    }
+}
